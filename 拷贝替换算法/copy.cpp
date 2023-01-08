e52d0e2d3893cc30e564819659213e12b53b072a
arrow <2638023977@qq.com>
2023-01-08 23:32:01 +0800

    拷贝算法

--- 拷贝替换算法/copy.cpp
+++ 拷贝替换算法/copy.cpp
@@ -3,6 +3,10 @@ using namespace std;
 #include<vector>
 #include<algorithm>
 
+void MyPrint(int val)
+{
+	cout << val << " ";
+}
 void test01()
 {
 	vector<int>v;
@@ -12,6 +16,9 @@ void test01()
 	}
 	vector<int>v2;
 	v2.resize(v.size());
+	copy(v.begin(), v.end(), v2.begin());
+	for_each(v2.begin(), v2.end(), MyPrint);
+	cout << endl;
 }
 int main()
 {
